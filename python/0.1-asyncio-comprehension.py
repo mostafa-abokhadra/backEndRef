ee9b02abbd185654c38c560ddbbbaf78a965edfe
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-10 23:51:19 +0300

    async comprehension ref

---
+++ python/0.1-asyncio-comprehension.py
@@ -0,0 +1,10 @@
+https://peps.python.org/pep-0530/
+https://www.blog.pythonlibrary.org/2017/02/14/whats-new-in-python-asynchronous-comprehensions-generators/
+https://stackoverflow.com/questions/42531143/how-to-type-hint-a-generator-in-python-3
+
+# Learing objectives
+"""
+    How to write an asynchronous generator
+    How to use async comprehensions
+    How to type-annotate generators
+"""
