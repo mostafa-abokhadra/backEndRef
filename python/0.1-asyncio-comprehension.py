91e5253354265c310eb82fdf8c917030db3e9308
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-11 02:31:39 +0300

    task 3 async comprehension

--- python/0.1-asyncio-comprehension.py
+++ python/0.1-asyncio-comprehension.py
@@ -122,3 +122,21 @@ from typing import List
 async_generator = __import__('./0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     return [_ async for _ in async_generator()]
+
+# code task 3
+"""
+    Import async_comprehension from the previous file and write a
+    measure_runtime coroutine that will execute async_comprehension
+    four times in parallel using asyncio.gather.
+    measure_runtime should measure the total runtime and return it.
+    Notice that the total runtime is roughly 10 seconds, explain it to yourself.
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+async def measure_runtime():
+    start = time.perf_counter()
+    await asyncio.gather(async_comprehension(), async_comprehension()
+        async_comprehension(), async_comprehension())
+    end = time.time()
+    return time.perf_counter() - start_time
