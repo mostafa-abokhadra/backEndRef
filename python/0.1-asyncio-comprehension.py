5452e4fafd67415c367cdf2b330a62574862adfa
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-11 02:11:14 +0300

    task 1 async comprehension

--- python/0.1-asyncio-comprehension.py
+++ python/0.1-asyncio-comprehension.py
@@ -95,3 +95,17 @@ from typing import Iterator
 def generate() -> Iterator[int]:
     for i in range(10):
         yield i
+
+# code task 1
+"""
+    Write a coroutine called async_generator that takes no arguments.
+    The coroutine will loop 10 times, each time asynchronously wait 1 second,
+    then yield a random number between 0 and 10. Use the random module.
+"""
+import random
+from typing import Generator
+import asyncio
+async def async_generator() -> Generator[float, None, None]:
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
