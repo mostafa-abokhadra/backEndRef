9769e85255374ccf2c7e22cc4043510f085db270
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-11 01:28:09 +0300

    updating comprehension

--- python/0.1-asyncio-comprehension.py
+++ python/0.1-asyncio-comprehension.py
@@ -23,6 +23,16 @@ result = [i async for i in aiter() if i % 2]
 # The PEP also makes it possible to use the await expressions in all kinds of comprehensions:
 result = [await fun() for fun in funcs]
 
+# Asynchronous comprehensions are only allowed inside an async def function.
+result = [await fun() for fun in funcs if await smth]
+result = {await fun() for fun in funcs if await smth} # set comprehension
+result = {fun: await fun() for fun in funcs if await smth} # dictionary comprehension
+result = {fun: await fun() async for fun in funcs if await smth} # dictionary comprehension
+
+
+dataset = {data for line in aiter()
+                async for data in line
+                if check(data)}
 """
     Basically you just need to add Python's new async keyword into your expression
     and call a callable that has implemented """__aiter__""". Trying to follow this
@@ -31,12 +41,29 @@ result = [await fun() for fun in funcs]
 >>> result = [i async for i in range(100) if i % 2]
 SyntaxError: invalid syntax
 
-# Asynchronous comprehensions are only allowed inside an async def function.
+import asyncio
+async def test(): 
+    return [i async for i in range(100) if i % 2]
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test())
 
-result = [await fun() for fun in funcs if await smth]
-result = {await fun() for fun in funcs if await smth} # set comprehension
-result = {fun: await fun() for fun in funcs if await smth} # dictionary comprehension
-result = {fun: await fun() async for fun in funcs if await smth} # dictionary comprehension
-dataset = {data for line in aiter()
-                async for data in line
-                if check(data)}
+"""
+    If you run this code, you will get a TypeError: async for requires an object with __aiter__ method,
+    got range. What you really want to do is call another async def function instead of calling range
+    directly. Here s an example:
+"""
+# file
+import asyncio
+async def numbers(numbers):
+    for i in range(numbers):
+        yield i
+        await asyncio.sleep(0.5)
+async def main():
+    odd_numbers = [i async for i in numbers(10) if i % 2]
+    print(odd_numbers)
+if __name__ == '__main__':
+    event_loop = asyncio.get_event_loop()
+    try:
+        event_loop.run_until_complete(main())
+    finally:
+        event_loop.close()
