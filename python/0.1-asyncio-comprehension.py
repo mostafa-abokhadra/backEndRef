a2c246f2f8d269429a2e3a3fe93f473f5c4246f6
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-11 01:25:05 +0300

    updating comprehension

--- python/0.1-asyncio-comprehension.py
+++ python/0.1-asyncio-comprehension.py
@@ -13,7 +13,6 @@ https://stackoverflow.com/questions/42531143/how-to-type-hint-a-generator-in-pyt
     Python has extensive support for synchronous comprehensions,
     allowing to produce lists, dicts, and sets with a simple and concise syntax.
 """
-
 # To illustrate the readability improvement, consider the following example
 result = []
 async for i in aiter():
@@ -24,9 +23,20 @@ result = [i async for i in aiter() if i % 2]
 # The PEP also makes it possible to use the await expressions in all kinds of comprehensions:
 result = [await fun() for fun in funcs]
 
+"""
+    Basically you just need to add Python's new async keyword into your expression
+    and call a callable that has implemented """__aiter__""". Trying to follow this
+    syntax will actually result in a SyntaxError though:
+"""
+>>> result = [i async for i in range(100) if i % 2]
+SyntaxError: invalid syntax
+
+# Asynchronous comprehensions are only allowed inside an async def function.
+
+result = [await fun() for fun in funcs if await smth]
+result = {await fun() for fun in funcs if await smth} # set comprehension
+result = {fun: await fun() for fun in funcs if await smth} # dictionary comprehension
+result = {fun: await fun() async for fun in funcs if await smth} # dictionary comprehension
 dataset = {data for line in aiter()
                 async for data in line
                 if check(data)}
-"""
-    Asynchronous comprehensions are only allowed inside an async def function.
-"""
