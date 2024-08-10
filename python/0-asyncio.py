70220172d4223d47f8f6797ba3d529de00d68aa8
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-10 23:53:02 +0300

    async ref

--- python/0-asyncio.py
+++ python/0-asyncio.py
@@ -1,3 +1,15 @@
+https://realpython.com/async-io-python/
+https://docs.python.org/3/library/asyncio.html
+https://docs.python.org/3/library/random.html #random.uniform
+
+## learning objectives
+"""
+    async and await syntax
+    How to execute an async program with asyncio
+    How to run concurrent coroutines
+    How to create asyncio tasks
+    How to use the random module
+"""
 import time
 start_time = time.time()
 time.sleep(4)
