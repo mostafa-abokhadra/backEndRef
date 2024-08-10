f1ec971176022a76df9ba87f77f4396f39bf67a6
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-11 01:12:02 +0300

    updating comprehension

--- python/0.1-asyncio-comprehension.py
+++ python/0.1-asyncio-comprehension.py
@@ -13,3 +13,13 @@ https://stackoverflow.com/questions/42531143/how-to-type-hint-a-generator-in-pyt
     Python has extensive support for synchronous comprehensions,
     allowing to produce lists, dicts, and sets with a simple and concise syntax.
 """
+
+# To illustrate the readability improvement, consider the following example
+result = []
+async for i in aiter():
+    if i % 2:
+        result.append(i)
+# With the proposed asynchronous comprehensions syntax, the above code becomes as short as:
+result = [i async for i in aiter() if i % 2]
+# The PEP also makes it possible to use the await expressions in all kinds of comprehensions:
+result = [await fun() for fun in funcs]
