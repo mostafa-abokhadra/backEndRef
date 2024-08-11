f1c0ddcbb30dc8c4893fe99dcf68c7ca7dcd0830
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-11 12:06:17 +0300

    string slicing

--- python/0.1-BasicsTo20.py
+++ python/0.1-BasicsTo20.py
@@ -11,3 +11,12 @@ mostafa
       abokhadra""") # using trible double qoute will print
                     # the string as your write exactly 
                     # including all new lines and spaces
+
+# slicing
+my_str = "mostafa abokhadra"
+print(my_str[0], my_str[-1])
+print(my_str[0:10]) # [start: end] , end not included
+print(my_str[0: 10: 2]) # [start: end: steps]
+print(my_str[:10])
+print(my_str[3:])
+print(my_str[:]) # the whole string
