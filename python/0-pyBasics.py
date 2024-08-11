d3b34ef8b5eca6d5f36e7264bc2d93cfe596ec09
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-11 03:43:48 +0300

    how to run pyFile

--- python/0-pyBasics.py
+++ python/0-pyBasics.py
@@ -1,4 +1,4 @@
-elzero 1 - 10
+# elzero 1 - 10
 # it can do anything :)
 """
     web: django, flask
@@ -13,4 +13,15 @@ elzero 1 - 10
 """
 # interpreted meaning the code is processed in run time
 # so no need for comile first like c lang for example, etc..
+# pyCharm is an IDE for python that you can use like VS
 
+# to run python file -> /bin/python filename.py or python filename.py 
+# or ./filename.py # but your file should include #!/usr/bin/python3
+# at the first line or better #!/use/bin/env python3
+
+# output
+# the only case that you can use semicolon in python is
+# when you write multiple commands at the same line
+print("hello mostafa ",end=""); print("abokhadra:)") # hello mostafa abokhadra:)
+# normally print function add a new line automatically ,
+# to disable this we use end=""
