569f4d12f6082012c28eaa92d64a408854ffefde
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-11 03:28:29 +0300

    python uses

---
+++ python/0-pyBasics.py
@@ -0,0 +1,16 @@
+elzero 1 - 10
+# it can do anything :)
+"""
+    web: django, flask
+    games: pyGame
+    desktop apps: pyGUI, Tkinter
+    Hacking
+    machine learnings, data science
+    ai, robots
+    automation of tasks
+    web scrapping (harvest)
+    android games
+"""
+# interpreted meaning the code is processed in run time
+# so no need for comile first like c lang for example, etc..
+
