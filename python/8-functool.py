a246890391c0796ff042588bc0960a66e04165c7
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-15 04:22:26 +0300

    functools.py

---
+++ python/8-functool.py
@@ -0,0 +1,5 @@
+"""
+    The functools module is for higher-order functions: functions that act on or return other functions.
+    In general, any callable object can be treated as a function for the purposes of this module.
+"""
+# https://www.youtube.com/watch?v=B9yM53qkbW0
